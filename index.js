978cd2cf0309eb68afabff45821a1aaee9169246
Sarah <spfohman@email.essex.edu>
2021-09-09 10:21:46 -0400

    commit tree a0a6f63ffaf241e18cd054e12976eeb662e601dc

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 
+
+//const wins = record.find(function(element){
+//	if (element.result==="W")
+//		return record.year;
+//});
+//console.log(wins);
+
+/*
+
+function superbowlWin(win, finder){
+    let result;
+	for(let n of win){
+		if(finder(n)===true){
+			result = item;
+			break;
+		}
+	}
+	return result;
+	
+}
+function isWin(result){
+	return record.result==='W';
+}
+superbowlWin(record, isWin);
+
+function superbowlWin(result){
+	return record.result==='W';
+}
+console.log(record.find(isYear));
+
+*/
+function superbowlWin(element){
+    const found =  element.find(e=>{
+        
+        console.log(e);
+        return e.result==='W';
+    });
+    if(found){
+        return found.year
+    }else{
+        return undefined;
+    }
+
+}

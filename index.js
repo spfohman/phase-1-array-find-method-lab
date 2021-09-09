e68f3685ab79e957ad08d623be22c5a747318fb6
Sarah <spfohman@email.essex.edu>
2021-09-09 10:35:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+
+function superbowlWin(element){
+    const found =  element.find(e=>{
+        
+        console.log(e);
+        return e.result==='W';
+    });
+    if(found){
+        return found.year
+    }else{
+        return undefined;
+    }
+
+}
